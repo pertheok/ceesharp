8b5614b130cf4fb36b7e4cea2d30376eaceb71f7
Jakub Slawecki <58791816+pertheok@users.noreply.github.com>
2023-02-21 23:58:20 +0000

    Add files via upload

---
+++ Digit to digit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.SquareDigits(9119));
+            Console.WriteLine(Kata.SquareDigits(765));
+        }
+    }
+}
---
+++ Narcissistic number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Narcissistic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // should return true
+            Console.WriteLine(Kata.Narcissistic(1));
+            // should return false
+            Console.WriteLine(Kata.Narcissistic(1652));
+            // should return true
+            Console.WriteLine(Kata.Narcissistic(371));
+        }
+    }
+}
