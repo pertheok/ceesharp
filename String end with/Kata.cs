a5fbe29926cae90a24ee89e075178337ddd4d954
Jakub Slawecki <kubaslawecki@gmail.com>
2023-03-01 22:46:21 +0000

    create new solution and a .gitignore

---
+++ String end with/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ends
+{
+    class Kata
+    {
+        public static bool Solution(string str, string ending)
+        {
+            if (str.Contains(ending))
+            {
+                string sub = str.Substring(str.Length - ending.Length);
+                if (sub == ending)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            // alternatively return str.EndsWith(ending);
+        }
+    }
+}
---
+++ String end with/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Solution("samurai", "ai"));
+            Console.WriteLine(Kata.Solution("samurai", "ra"));
+            Console.WriteLine(Kata.Solution("abcabc", "bc"));
+        }
+    }
+}
