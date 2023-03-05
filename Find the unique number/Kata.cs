cdfae22caf3718b8d9d403e4815657f79c6fe893
Jakub Slawecki <kubaslawecki@gmail.com>
2023-03-05 19:28:32 +0000

    add partially completed challenge

---
+++ Find the unique number/Kata.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unique
+{
+    public class Kata
+    {
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            int startPointer = numbers.First();
+            int endPointer = numbers.Last();
+            // Below is an attempt to work the edge case where the unique item is either the first or the last in the array
+            // if (startPointer != endPointer)
+            // {
+            //     foreach (int number in numbers)
+            //     {
+            //         if (endPointer == number)
+            //         {
+            //             return startPointer;
+            //         }
+            //         else
+            //         {
+            //             return endPointer;
+            //         }
+            //     }
+            // }
+            // else
+            // {
+                foreach (int number in numbers)
+                {
+                    if (startPointer == number)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        startPointer = number;
+                        return startPointer;
+                    }
+                }
+            // }
+            return startPointer;
+        }
+    }
+}
---
+++ Find the unique number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Unique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GetUnique(new [] {1, 2, 2, 2}));
+            Console.WriteLine(Kata.GetUnique(new [] {-2, 2, 2, 2}));
+            Console.WriteLine(Kata.GetUnique(new [] {11, 11, 14, 11, 11}));
+        }
+    }
+}
